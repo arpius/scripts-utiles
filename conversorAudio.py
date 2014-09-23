603f0c102536712f5a97a49f2de5401276efe945
arpius <amurriokoa@gmail.com>
2014-09-23 12:31:15 +0200

    agrego una función al script

--- conversorAudio.py
+++ conversorAudio.py
@@ -7,46 +7,56 @@
 import os
 
 def menu():
-	os.system("clear")
-	print("\n--- MENÚ ---\n1) Sustituir espacios en blanco por guiones bajos.\n2) Convertir de un formato de audio a otro.\n3) Salir.")
+    os.system("clear")
+    print("\n--- MENÚ ---\n1) Listar el contenido de la carpeta. \n2) Sustituir espacios en blanco por guiones bajos.\n3) Convertir de un formato de audio a otro.\n4) Salir.")
+
+def listar():
+	os.system("ls -lah")
+	input("\nPulsa intro para continuar...")
 
 def renombrar():
-	files = os.listdir() #listamos el contenido del directorio
+    files = os.listdir()  # listamos el contenido del directorio
 
-	for file in files:
-		os.rename(file, "_".join(file.split())) #sustituimos los posibles espacios en blanco por guiones bajos y renombramos el archivo
+    for file in files:
+        # sustituimos los posibles espacios en blanco por guiones bajos y
+        # renombramos el archivo
+        os.rename(file, "_".join(file.split()))
 
 def convertir(origen, destino):
-	files = os.listdir()
+    files = os.listdir()
 
-	for file in files:
-		if os.path.isfile(file): #si es un archivo
-			if file.endswith(origen) == True: #si la extension es 'origen'
-				print(file+ " se convertirá a " +destino)
-					
-				os.system("ffmpeg -i {0} {0}.{1}".format(file, destino)) #usamos ffmpeg para convertir entre formatos de audio
+    for file in files:
+        if os.path.isfile(file):  # si es un archivo
+            if file.endswith(origen) == True:  # si la extension es 'origen'
+                print(file + " se convertirá a " + destino)
+
+                # usamos ffmpeg para convertir entre formatos de audio
+                os.system("ffmpeg -i {0} {0}.{1}".format(file, destino))
 
 def corregir(origen, destino):
-	files = os.listdir()
+    files = os.listdir()
 
-	for file in files:
-		if os.path.isfile(file):
-			if file.endswith(destino) == True:
-				os.rename(file, file.replace(".{0}.".format(origen), ".")) #eliminamos la extensión antigua del nombre final
+    for file in files:
+        if os.path.isfile(file):
+            if file.endswith(destino) == True:
+                # eliminamos la extensión antigua del nombre final
+                os.rename(file, file.replace(".{0}.".format(origen), "."))
 
 while True:
-	menu()
-	opcion = input("\nElige una opción: ")
-	
-	if opcion == "1":
-		renombrar()
-	elif opcion == "2":
-		origen = input("Formato de origen: ")
-		destino = input("Formato de destino: ")
-		convertir(origen, destino)
-		corregir(origen, destino)
-	elif opcion == "3":
-		print("Aguuur")
-		break
-	else:
-		print("Opción no válida")
+    menu()
+    opcion = input("\nElige una opción: ")
+
+    if opcion == "1":
+    	listar()
+    elif opcion == "2":
+        renombrar()
+    elif opcion == "3":
+        origen = input("Formato de origen: ")
+        destino = input("Formato de destino: ")
+        convertir(origen, destino)
+        corregir(origen, destino)
+    elif opcion == "4":
+        print("Aguuur")
+        break
+    else:
+        print("Opción no válida")
