eb2d72c21d7efd34a5f220c0558144c025635845
arpius <amurriokoa@gmail.com>
2015-09-18 11:50:14 +0200

    agrego más puertos al script

--- hosts_en_pie.py
+++ hosts_en_pie.py
@@ -4,17 +4,17 @@
 import nmap
 
 nm = nmap.PortScanner()
-hosts = input('IP o rango de IPs a escanear: ')
+nodos = input('IP o rango de IPs a escanear: ')
 
-while len(hosts) == 0:
-    hosts = input('IP o rango de IPs a escanear: ')
+while len(nodos) == 0:
+    nodos = input('IP o rango de IPs a escanear: ')
 
-nm.scan(hosts=hosts, arguments='-n -sP -PE -PA21,23,80,3389')
+nm.scan(hosts=nodos, arguments='-n -sP -PE -PA21,22,23,53,80,137,138,139,443,445,631,3389')
 listado = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 archivo = open('escaneado.txt', 'w')
 
-for host, estado in listado:
-    print(host, estado)
-    archivo.write(host+ '\n')
+for nodo, estado in listado:
+    print(nodo, estado)
+    archivo.write(nodo+ '\n')
 
 archivo.close()
