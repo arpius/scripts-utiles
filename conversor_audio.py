2b846a70a30d42bddce44f6fad962ee05c5b1146
arpius <amurriokoa@gmail.com>
2015-12-02 20:26:00 +0100

    sustituyo os.system por subprocess

--- conversor_audio.py
+++ conversor_audio.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 # Script para convertir pistas de audio de un formato a otro.
 # Los archivos de audio deben estar en el mismo directorio.
 # Este script está escrito en python 3.
 
 import os
+import subprocess
+
 
 def menu():
-    os.system("clear")
-    print("\n--- MENÚ ---\n1) Listar el contenido de la carpeta. \n2) Sustituir espacios en blanco por guiones bajos.\n3) Convertir de un formato de audio a otro.\n4) Salir.")
+    subprocess.call("clear")
+
+    titulo = ' Menú '
+    op1 = '1) Listar el contenido de la carpeta.'
+    op2 = '2) Sustituir espacios en blanco por guiones bajos.'
+    op3 = '3) Convertir de un formato de audio a otro.'
+    op4 = '4) Salir.'
+
+    menu = '{:-^52}\n {}\n {}\n {}\n {}'.format(titulo, op1, op2, op3, op4)
+
+    print(menu)
+
 
 def listar():
-	os.system("ls -lah")
-	input("\nPulsa intro para continuar...")
+    subprocess.call(["ls", "-lh"])
+    input("\nPulsa intro para continuar...")
+
 
 def renombrar():
     files = os.listdir()  # listamos el contenido del directorio
@@ -22,23 +36,25 @@ def renombrar():
         # renombramos el archivo
         os.rename(file, "_".join(file.split()))
 
+
 def convertir(origen, destino):
     files = os.listdir()
 
     for file in files:
         if os.path.isfile(file):  # si es un archivo
-            if file.endswith(origen) == True:  # si la extension es 'origen'
-                print(file + " se convertirá a " + destino)
+            if file.endswith(origen) is True:  # si la extension es 'origen'
+                print("{} se convertirá a {}".format(file, destino))
 
                 # usamos ffmpeg para convertir entre formatos de audio
-                os.system("ffmpeg -i {0} {0}.{1}".format(file, destino))
+                subprocess.call(["ffmpeg", "-i", file, "{}.{}".format(file, destino)])
+
 
 def corregir(origen, destino):
     files = os.listdir()
 
     for file in files:
         if os.path.isfile(file):
-            if file.endswith(destino) == True:
+            if file.endswith(destino) is True:
                 # eliminamos la extensión antigua del nombre final
                 os.rename(file, file.replace(".{0}.".format(origen), "."))
 
@@ -47,7 +63,7 @@ while True:
     opcion = input("\nElige una opción: ")
 
     if opcion == "1":
-    	listar()
+        listar()
     elif opcion == "2":
         renombrar()
     elif opcion == "3":
--- conversor_ebooks.py
+++ conversor_ebooks.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 # Script para convertir libros electrónicos a otro formato.
 # Calibre debe estar instalado.
 
+import subprocess
 import os
 
+
 def menu():
-    os.system("clear")
-    print("\n--- MENÚ ---\n1) Listar el contenido de la carpeta.\n2) Sustituir espacios en blanco por guiones bajos.\n3) Convertir ebook a otro formato.\n4) Salir.")
+    subprocess.call("clear")
+
+    titulo = ' Menú '
+    op1 = '1) Listar el contenido de la carpeta.'
+    op2 = '2) Sustituir espacios en blanco por guiones bajos.'
+    op3 = '3) Convertir ebook a otro formato.'
+    op4 = '4) Salir.'
+
+    menu = '{:-^52}\n {}\n {}\n {}\n {}'.format(titulo, op1, op2, op3, op4)
+
+    print(menu)
+
 
 def listar():
-	os.system("ls -l")
-	input("\nPulsa intro para continuar...")
+    subprocess.call(["ls", "-lh"])
+    input("\nPulsa intro para continuar...")
+
 
 def renombrar():
     files = os.listdir()  # listamos el contenido del directorio
@@ -21,32 +35,34 @@ def renombrar():
         # renombramos el archivo
         os.rename(file, "_".join(file.split()))
 
+
 def convertir(origen, destino):
     files = os.listdir()
 
     for file in files:
         if os.path.isfile(file):  # si es un archivo
-            if file.endswith(origen) == True:  # si la extension es 'origen'
-                print(file + " se convertirá a " + destino)
+            if file.endswith(origen) is True:  # si la extension es 'origen'
+                print("{} se convertirá a {}".format(file, destino))
 
                 # usamos calibre para convertir a otro formato
-                os.system("ebook-convert {0} {0}.{1}".format(file, destino))
+                subprocess.call(["ebook-convert", file, "{}.{}".format(file, destino)])
+
 
 def corregir(origen, destino):
     files = os.listdir()
 
     for file in files:
         if os.path.isfile(file):
-            if file.endswith(destino) == True:
+            if file.endswith(destino) is True:
                 # eliminamos la extensión antigua del nombre final
-                os.rename(file, file.replace(".{0}.".format(origen), "."))
+                os.rename(file, file.replace(".{}.".format(origen), "."))
 
 while True:
     menu()
     opcion = input("\nElige una opción: ")
 
     if opcion == "1":
-    	listar()
+        listar()
     elif opcion == "2":
         renombrar()
     elif opcion == "3":
