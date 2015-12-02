e216e5d3f9806a9ad417d81e58af5646d84c05b4
arpius <amurriokoa@gmail.com>
2015-12-02 20:37:33 +0100

    sustituyo os.system por subprocess

--- resize_imgs.py
+++ resize_imgs.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Script para redimensionar imágenes para subirlas a la web o mandar por correo.
-# Imagemagick debe estar instalado.
-# mogrify -resize ANCHOxALTO [-quality CALIDAD] ruta_de_las_imagenes/*.EXTENSIÓN
+
+"""
+Script que redimensiona imágenes para subirlas a la web o mandar por correo.
+Imagemagick debe estar instalado.
+mogrify -resize ANCHOxALTO [-quality CALIDAD] ruta_de_las_imagenes/*.EXTENSIÓN
+"""
 
 import os
+import subprocess
 
 print("Introduce la ruta completa de la imágen que quieres redimensionar")
-print("En caso de que quieras redimensionar todas las imágenes de un mismo tipo escribe: ruta_imagenes/*.extensión")
+print("En caso de que quieras redimensionar todas las imágenes de un mismo"
+      "tipo escribe: ruta_imagenes/*.extensión")
 print("¡OJOCUIDAO este script SOBRESCRIBE la imagen original!")
 
 ruta_img = input("Ruta de la imágen: ")
 
-os.system("mogrify -resize 1024x768 -quality 94% {0}".format(ruta_img))
+subprocess.call(["mogrify", "-resize", "1024x768", "-quality", "94%", ruta_img])
