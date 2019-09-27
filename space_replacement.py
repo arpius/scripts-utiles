d2c78cd70c4b75ce3949f156d3d5832cf70e3e96
Aritz <aritz@kubo>
2019-09-27 21:10:01 +0200

    modify to use argparse

--- space_replacement.py
+++ space_replacement.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
+import argparse
 import os
+import sys
 
 
-path = sys.argv[1]
-replacement = sys.argv[2]
+parser = argparse.ArgumentParser(prog='space_replacement',
+                                 usage='%(prog)s path replacement',
+                                 description='Replaces spaces in a file name.')
+
+parser.add_argument('Path',
+                    metavar='path',
+                    type=str,
+                    help='the files path')
+parser.add_argument('Replacement',
+                    metavar='replacement',
+                    type=str,
+                    help='the replacement character')
+
+args = parser.parse_args()
+path = args.Path
+replacement = args.Replacement
+
+if not os.path.isdir(path):
+    print("The specified path does not exist.")
+    sys.exit()
+
 files = os.listdir(path)
 
-
 for file in files:
-    old = os.path.join(path, file)
-    new = replacement.join(old.split())
+    old_name = os.path.join(path, file)
+    new_name = replacement.join(old_name.split())
 
-    os.rename(old, new)
+    os.rename(old_name, new_name)
