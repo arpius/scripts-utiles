904c1f4b896b190fc9b22157a3646d5126bdaab9
arpius <aritz.pius@anche.no>
2019-11-04 11:08:59 +0100

    add main function

--- backup.py
+++ backup.py
@@ -54,33 +54,38 @@ def menu():
     print(menu)
 
 
-while True:
-    menu()
-    option = input("\nChoose an option: ")
-    continue_text = "Press the <ENTER> key to continue..."
+def main():
+    while True:
+        menu()
+        option = input("\nChoose an option: ")
+        continue_text = "Press the <ENTER> key to continue..."
 
-    if option == '1':
-        show()
-        input(continue_text)
-    elif option == '2':
-        target = input("Insert backup's target: ")
-        backup(target=target)
-        input(continue_text)
-    elif option == '3':
-        target = input("Insert the ID of the snapshot to be deleted: ")
-        remove(target=target)
-        input(continue_text)
-    elif option == '4':
-        copies = input("Insert the number of copies to be kept: ")
-        prune(copies=copies)
-        input(continue_text)
-    elif option == '5':
-        snapshot = input("Insert the ID of the snapshot to be restored: ")
-        target = input("Insert where it will be restored: ")
-        restore(snapshot=snapshot, target=target)
-        input(continue_text)
-    elif option == '6':
-        print("See you!")
-        break
-    else:
-        print("Incorrect option!")
+        if option == '1':
+            show()
+            input(continue_text)
+        elif option == '2':
+            target = input("Insert backup's target: ")
+            backup(target=target)
+            input(continue_text)
+        elif option == '3':
+            target = input("Insert the ID of the snapshot to be deleted: ")
+            remove(target=target)
+            input(continue_text)
+        elif option == '4':
+            copies = input("Insert the number of copies to be kept: ")
+            prune(copies=copies)
+            input(continue_text)
+        elif option == '5':
+            snapshot = input("Insert the ID of the snapshot to be restored: ")
+            target = input("Insert where it will be restored: ")
+            restore(snapshot=snapshot, target=target)
+            input(continue_text)
+        elif option == '6':
+            print("See you!")
+            break
+        else:
+            print("Incorrect option!")
+
+
+if __name__ == "__main__":
+    main()
